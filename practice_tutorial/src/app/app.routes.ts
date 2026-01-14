99fa86b58d84a9fc368a690a11a6f2c8b86fce82
MdAnsari24 <mdalhussainansari@gmail.com>
2026-01-15 00:52:49 +0530

    frontpage align1

--- practice_tutorial/src/app/app.routes.ts
+++ practice_tutorial/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Products } from './products/products';
+import { Other } from './other/other';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'products', component: Products },
+  { path: 'other', component: Other },
 ];
---
+++ practice_tutorial/src/app/other/other.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other',
+  imports: [],
+  templateUrl: './other.html',
+  styleUrl: './other.scss',
+})
+export class Other {
+
+}
